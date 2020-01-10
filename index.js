f93fd6a13a5ec94ea0c1a7b7f0d615a3a9e3838a
Keylee <lineberry.kelly@gmail.com>
2020-01-09 20:55:58 -0700

    added links for fs & axios. Also wrote more psuedo code, working on collecting the information I need from the GitHub API to pull all information needed for the profile pdfs.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-//Prompt user with Questions
-var inquirer = require("inquirer");
+//Link require links
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
 
+//Prompt user with Questions
 inquirer
     .prompt([
         {
@@ -12,24 +15,31 @@ inquirer
             type: "list",
             message: "What is your favorite color?",
             choices: ['red', 'yellow', 'orange', 'green', 'blue', 'purple', 'pink', 'grey', 'black'],
-            name: "color"
-            
+            name: "color"  
         },
-    ]);
+    ])
+    .then(function(response) {
+        console.log(response.username);
+        console.log(response.color);
+        // return  `# GET /users/${response.username}`;
+        // return  `curl -i http://api.github.com/users/${response.username}`;
+    });
 
-//Fetch GitHub user information
-    //Connect to the GitHub API 
-    //Throw error if the username is not found
+        
+        //write a then function that Fetchs the response.username and inserts it in the GitHub API info
+            //Pull User's Picture
+            //Pull User's Name
+            //Pull User's # of Repos
+                // # GET /users/${response.username};
+                // `curl -i https://api.github.com/users/${response.username}/repos`
+            //Pull User's # of Followers
+            //Pull User's # of Followed
+            //Pull USer's # of Starred 
+                // return `curl -i http://api.github.com/users/${response.username}`
+        //write a function that pulls the User's location based off Google Maps
+
+
 
 //Generate pdf that opens in browser
-    // //PDF must include:
-        // Bio image that is pulled from GitHub
-        // Users Location based off Google Location
-        // Link to GitHub Profile 
-        //Include # of
-            // Public Repos 
-            // Followers
-            // Stared 
-            // Following 
 
 //Create themes based off favorite color option
