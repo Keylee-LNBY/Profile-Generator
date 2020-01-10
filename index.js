1fb571efd09d1cf120d36bbc0549677402d43179
Keylee <lineberry.kelly@gmail.com>
2020-01-09 19:03:40 -0700

    added psuedo code

---
+++ index.js
@@ -0,0 +1,8 @@
+//Prompt user with Questions
+
+//Fetch GitHub user information
+    //Throw error if the username is not found
+
+//Generate pdf that opens in browser
+
+//Create themes based off favorite color option
