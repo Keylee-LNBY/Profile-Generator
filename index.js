8485a51d33eae4cfa7f140bcee1d64489ba5282a
Keylee <lineberry.kelly@gmail.com>
2020-01-12 10:05:50 -0700

    added the html framework, colors framework, and API information.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
+const pdf = require("html-pdf");
 
 //Prompt user with Questions
 inquirer
@@ -14,28 +15,216 @@ inquirer
         {
             type: "list",
             message: "What is your favorite color?",
-            choices: ['red', 'yellow', 'orange', 'green', 'blue', 'purple', 'pink', 'grey', 'black'],
+            choices: ['green', 'blue', 'pink', 'red'],
             name: "color"  
         },
     ])
-    .then(function(response) {
-        console.log(response.username);
-        console.log(response.color);
-        // return  `# GET /users/${response.username}`;
-        // return  `curl -i http://api.github.com/users/${response.username}`;
+    .then(function (response) {
+        console.log(response);
+        gitHub(response.repo);
     });
+        
+//end of asking color question in Terminal
+function gitHub(userName){
+    const queryUrl = `https://api.github.com/users/${userName}?`;
+
+    axios.get(queryUrl)
+    .then(function (response) {
+        // handle success
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+}
+
+const colors = {
+    green: {
+      wrapperBackground: "#E6E1C3",
+      headerBackground: "#C1C72C",
+      headerColor: "black",
+      photoBorderColor: "#black"
+    },
+    blue: {
+      wrapperBackground: "#5F64D3",
+      headerBackground: "#26175A",
+      headerColor: "white",
+      photoBorderColor: "#73448C"
+    },
+    pink: {
+      wrapperBackground: "#879CDF",
+      headerBackground: "#FF8374",
+      headerColor: "white",
+      photoBorderColor: "#FEE24C"
+    },
+    red: {
+      wrapperBackground: "#DE9967",
+      headerBackground: "#870603",
+      headerColor: "white",
+      photoBorderColor: "white"
+    }
+  };
+  
+function generateHTML(data) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+        <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
+        <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
+        <title>Document</title>
+        <style>
+            @page {
+                margin: 0;
+            }
+            *,
+            *::after,
+            *::before {
+            box-sizing: border-box;
+            }
+            html, body {
+            padding: 0;
+            margin: 0;
+            }
+            html, body, .wrapper {
+            height: 100%;
+            }
+            .wrapper {
+            background-color: ${colors[data.color].wrapperBackground};
+            padding-top: 100px;
+            }
+            body {
+            background-color: white;
+            -webkit-print-color-adjust: exact !important;
+            font-family: 'Cabin', sans-serif;
+            }
+            main {
+            background-color: #E9EDEE;
+            height: auto;
+            padding-top: 30px;
+            }
+            h1, h2, h3, h4, h5, h6 {
+            font-family: 'BioRhyme', serif;
+            margin: 0;
+            }
+            h1 {
+            font-size: 3em;
+            }
+            h2 {
+            font-size: 2.5em;
+            }
+            h3 {
+            font-size: 2em;
+            }
+            h4 {
+            font-size: 1.5em;
+            }
+            h5 {
+            font-size: 1.3em;
+            }
+            h6 {
+            font-size: 1.2em;
+            }
+            .photo-header {
+            position: relative;
+            margin: 0 auto;
+            margin-bottom: -50px;
+            display: flex;
+            justify-content: center;
+            flex-wrap: wrap;
+            background-color: ${colors[data.color].headerBackground};
+            color: ${colors[data.color].headerColor};
+            padding: 10px;
+            width: 95%;
+            border-radius: 6px;
+            }
+            .photo-header img {
+            width: 250px;
+            height: 250px;
+            border-radius: 50%;
+            object-fit: cover;
+            margin-top: -75px;
+            border: 6px solid ${colors[data.color].photoBorderColor};
+            box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
+            }
+            .photo-header h1, .photo-header h2 {
+            width: 100%;
+            text-align: center;
+            }
+            .photo-header h1 {
+            margin-top: 10px;
+            }
+            .links-nav {
+            width: 100%;
+            text-align: center;
+            padding: 20px 0;
+            font-size: 1.1em;
+            }
+            .nav-link {
+            display: inline-block;
+            margin: 5px 10px;
+            }
+            .workExp-date {
+            font-style: italic;
+            font-size: .7em;
+            text-align: right;
+            margin-top: 10px;
+            }
+            .container {
+            padding: 50px;
+            padding-left: 100px;
+            padding-right: 100px;
+            }
+
+            .row {
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: space-between;
+                margin-top: 20px;
+                margin-bottom: 20px;
+            }
+
+            .card {
+                padding: 20px;
+                border-radius: 6px;
+                background-color: ${colors[data.color].headerBackground};
+                color: ${colors[data.color].headerColor};
+                margin: 20px;
+            }
+            
+            .col {
+            flex: 1;
+            text-align: center;
+            }
+
+            a, a:hover {
+            text-decoration: none;
+            color: inherit;
+            font-weight: bold;
+            }
+
+            @media print { 
+            body { 
+                zoom: .75; 
+            } 
+            }
+        </style>`
+            }
 
         
-        //write a then function that Fetchs the response.username and inserts it in the GitHub API info
+        //write a then function that Fetchs the data.username and inserts it in the GitHub API info
             //Pull User's Picture
             //Pull User's Name
             //Pull User's # of Repos
-                // # GET /users/${response.username};
-                // `curl -i https://api.github.com/users/${response.username}/repos`
+                // # GET /users/${data.username};
+                // `curl -i https://api.github.com/users/${data.username}/repos`
             //Pull User's # of Followers
             //Pull User's # of Followed
             //Pull USer's # of Starred 
-                // return `curl -i http://api.github.com/users/${response.username}`
+                // return `curl -i http://api.github.com/users/${data.username}`
         //write a function that pulls the User's location based off Google Maps
 
 
