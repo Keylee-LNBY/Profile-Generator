ed08a575d5b0000d752c9d59e00e994d41d8088e
Keylee <lineberry.kelly@gmail.com>
2020-01-15 23:18:33 -0700

    added a gitignore file and edited the location for the styling info

--- index.js
+++ index.js
@@ -7,6 +7,33 @@ const util = require("util");
 
 const htmlAsyn = util.promisify(fs.writeFile);
 
+const colors = {
+    green: {
+      wrapperBackground: "#E6E1C3",
+      headerBackground: "#C1C72C",
+      headerColor: "black",
+      photoBorderColor: "#black"
+    },
+    blue: {
+      wrapperBackground: "#5F64D3",
+      headerBackground: "#26175A",
+      headerColor: "white",
+      photoBorderColor: "#73448C"
+    },
+    pink: {
+      wrapperBackground: "#879CDF",
+      headerBackground: "#FF8374",
+      headerColor: "white",
+      photoBorderColor: "#FEE24C"
+    },
+    red: {
+      wrapperBackground: "#DE9967",
+      headerBackground: "#870603",
+      headerColor: "white",
+      photoBorderColor: "white"
+    }
+};
+
 
 //Prompt user with Questions
 inquirer
@@ -25,32 +52,6 @@ inquirer
     ])
     .then(function (response) {
         console.log(response);
-        const colors = {
-            'green': {
-              wrapperBackground: "#E6E1C3",
-              headerBackground: "#C1C72C",
-              headerColor: "black",
-              photoBorderColor: "#black"
-            },
-            'blue': {
-              wrapperBackground: "#5F64D3",
-              headerBackground: "#26175A",
-              headerColor: "white",
-              photoBorderColor: "#73448C"
-            },
-            'pink': {
-              wrapperBackground: "#879CDF",
-              headerBackground: "#FF8374",
-              headerColor: "white",
-              photoBorderColor: "#FEE24C"
-            },
-            'red': {
-              wrapperBackground: "#DE9967",
-              headerBackground: "#870603",
-              headerColor: "white",
-              photoBorderColor: "white"
-            }
-        };
         gitHub(response.username);
     });
 
